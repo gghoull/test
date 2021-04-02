d62e17cab56cf959102253d30b722435a4163fc1
GHOUL <547339033@qq.com>
2021-04-02 10:28:04 +0800

    测试=====

--- src/main/java/com/gyl/service/UserService.java
+++ src/main/java/com/gyl/service/UserService.java
@@ -5,4 +5,6 @@ package com.gyl.service;
  * @date 2021-04-2021/4/2 10:14:49
  */
 public interface UserService {
+    //执行事务
+    public void tranfor();
 }
---
+++ src/main/java/com/gyl/service/impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.gyl.service.impl;
+
+import com.gyl.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 高云雷
+ * @date 2021-04-2021/4/2 10:24:44
+ */
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserService userService;
+    @Override
+    public void tranfor() {
+        //更新一条数据
+
+        //抛出异常
+
+
+        //删除一条数据
+    }
+}
